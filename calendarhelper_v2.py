68395c21a3d4d6b0f61834950aa4f6212c4cb584
adamddh <58528419+adamddh@users.noreply.github.com>
2020-05-04 21:36:08 -0400

    comments

--- calendarhelper_v2.py
+++ calendarhelper_v2.py
@@ -24,8 +24,8 @@ months = {
 #     "Lavender"  : 1
 #     "Sage"      : 2
 #     "Grape"     : 3
-#     "Flamingo"  : 4 #changes made by me
-#     "Banana"    : 5 #holla
+#     "Flamingo"  : 4
+#     "Banana"    : 5
 #     "Tangerine" : 6
 #     "Peacock"   : 7
 #     "Grapite"   : 8
