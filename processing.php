56b8b7cde87a3af9d407934cfb0019b8624981b4
Adam DenHaan <adamdenhaan@The-Real-Adams-MBP.local>
2020-07-10 16:10:14 -0400

    write to file (unsuccessful, permissions?)

--- processing.php
+++ processing.php
@@ -1,2 +1,28 @@
 <?php
+
+
+$calendar_colors = [
+    "Lavender"  => 1,
+    "Sage"      => 2,
+    "Grape"     => 3,
+    "Flamingo"  => 4,
+    "Banana"    => 5,
+    "Tangerine" => 6,
+    "Peacock"   => 7,
+    "Grapite"   => 8,
+    "Blueberry" => 9,
+    "Basil"     => 10,
+    "Tomato"    => 11,
+];
+
+$cal_color = $calendar_colors[$_POST['cal_color']];
+$initials = $_POST['initals'];
+$email = $_POST['email'];
+$string = "Kachow!";
+$myfile = fopen("list.py", "w") or die("Unable to open file!");
+fwrite($myfile, $string);
+
+
+echo "Hello all";
+
 ?>
